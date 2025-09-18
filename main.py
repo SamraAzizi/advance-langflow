b1137af78eedd38557064fc2763fa869cdfa1f0f
SamraAzizi <samarraazizi@gmail.com>
2025-09-18 15:55:40 +0500

    advance langflow web

--- main.py
+++ main.py
@@ -22,3 +22,45 @@ class State(TypedDict):
     bing_analysis: str | None
     reddit_analysis: str | None
     final_answer: str | None
+
+
+def google_search(state: State):
+    return 
+
+
+def google_search(state: State):
+    return 
+
+
+def google_search(state: State):
+    return 
+
+
+def google_search(state: State):
+    return 
+
+
+
+def google_search(state: State):
+    return 
+
+
+def google_search(state: State):
+    return 
+
+
+
+def google_search(state: State):
+    return 
+
+
+def google_search(state: State):
+    return 
+
+
+
+def google_search(state: State):
+    return 
+
+def google_search(state: State):
+    return 

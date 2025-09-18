ad99661db61f98668764e6c1005b9db8956ee721
SamraAzizi <samarraazizi@gmail.com>
2025-09-18 15:35:48 +0500

    advance langflow web

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from lang!")
+from dotenv import load_dotenv
+from typing import Annotated
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
 
-
-if __name__ == "__main__":
-    main()
+load_dotenv()

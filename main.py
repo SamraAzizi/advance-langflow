ab567f3ad7007bbfeee74a36c045707aa90dd696
SamraAzizi <samarraazizi@gmail.com>
2025-09-18 15:53:19 +0500

    advance langflow web

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ from typing_extensions import TypedDict
 from pydantic import BaseModel, Field
 
 load_dotenv()
+
+llm = init_chat_model("gpt-4o")
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+    user_question : str |None
+    google_results: str | None
+    bing_results: str | None
+    reddit_results: str | None
+    selected_reddit_urls: list[str] | None
+    reddit_post_data: list | None
+    google_analysis: str | None
+    bing_analysis: str | None
+    reddit_analysis: str | None
+    final_answer: str | None
